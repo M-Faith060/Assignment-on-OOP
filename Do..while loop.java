4625f2814827acfa3829eeb329e6b149261c0696
M-Faith060 <159757364+M-Faith060@users.noreply.github.com>
2024-10-15 20:30:17 +0300

    Add files via upload

---
+++ Do..while loop.java
@@ -0,0 +1,11 @@
+public class LoopExample{
+    public static void main(String[] args){
+        int count = 1;
+
+        // do...while loop
+        Sytem.out.println("\nDo...while loop:");do{
+            Sytem.out.println("count is:" + count);count ++;
+            while (count < 1);
+        }
+    }
+}
